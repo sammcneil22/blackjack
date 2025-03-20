9d282993b6c66ca5c0ea06d08806993760d5701a
sammcneil22 <w0507433@campus.nscc.ca>
2025-03-20 14:17:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -20,10 +20,7 @@ const playerTotalDiv = document.getElementById("playerTotal");
 const dealerTotalDiv = document.getElementById("dealerTotal");
 
 
-txt = "a";
-while(1){
-    txt = txt += "a";    //add as much as the browser can handle
-}
+
 //[evil laugh] BOOM! All memory used up, and it is now CRASHED!
 
 // Initialize deck
